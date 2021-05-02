2d53237b52dfc340609b4716b4aa8f2ea6a53ab8
Deepika Bekal <deepika.bekal2@gmail.com>
2021-05-02 23:28:22 +0400

    fixed Engineer class test

--- __tests__/Engineer.test.js
+++ __tests__/Engineer.test.js
@@ -2,7 +2,7 @@ const Engineer = require('../lib/Engineer');
 
 //test to check if the engineer object is created
 test('create engineer object', () => {
-    const engineer = new Engineer();
+    const engineer = new Engineer('Jane', 1, 'email','janehub');
 
     expect(engineer.github).toEqual(expect.any(String));
 });
